1685a2b11b53f973d3fb61bfb8b22ae054ca9a54
Charlotte <charlotte.mauvezin@gmail.com>
2020-10-19 16:59:24 +0200

    Setup

---
+++ server/routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const bcrypt = require('bcrypt')
+const { Client } = require('pg')
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'pouet',
+    database: 'TP5'
+})
+
+client.connect()
+
+module.exports = router
